cc29062d23abbba20214a7e1387b1b97ed7ad6a4
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-06-24 10:30:55 -0300

    feat: adds Medico constructor to Medico class

--- src/main/java/med/voll/api/medico/Medico.java
+++ src/main/java/med/voll/api/medico/Medico.java
@@ -26,4 +26,13 @@ public class Medico {
 
     @Embedded
     private Direccion direccion;
+
+    public Medico(DatosRegistroMedico datos) {
+        this.id = null;
+        this.nombre = datos.nombre();
+        this.email = datos.email();
+        this.documento = datos.documento();
+        this.especialidad = datos.especialidad();
+        this.direccion = new Direccion(datos.datosdireccion());
+    }
 }
