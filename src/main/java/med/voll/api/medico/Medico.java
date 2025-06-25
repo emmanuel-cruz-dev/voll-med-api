e0b397246ab0fea732aa51671258ceb3783184aa
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-06-25 08:46:19 -0300

    feat: adds actualizarInformaciones method to Medico class

--- src/main/java/med/voll/api/medico/Medico.java
+++ src/main/java/med/voll/api/medico/Medico.java
@@ -1,6 +1,7 @@
 package med.voll.api.medico;
 
 import jakarta.persistence.*;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -36,5 +37,17 @@ public class Medico {
         this.documento = datos.documento();
         this.especialidad = datos.especialidad();
         this.direccion = new Direccion(datos.direccion());
+    }
+
+    public void actualizarInformaciones(@Valid DatosActualizacionMedico datos) {
+        if(datos.nombre() != null){
+            this.nombre = datos.nombre();
+        }
+        if(datos.telefono() != null){
+            this.telefono = datos.telefono();
+        }
+        if(datos.direccion() != null){
+            this.direccion.actualizarDireccion(datos.direccion());
+        }
     }
 }
