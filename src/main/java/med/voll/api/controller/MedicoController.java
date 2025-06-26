d62b82405f885bfd96a6f32a143c665f8508d23a
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-06-26 12:48:15 -0300

    feat: adds ResponseEntity and updates registrar, listar, actualizar and eliminar methods in MedicoController class

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -6,12 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 import org.springframework.data.web.PagedResourcesAssembler;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.PagedModel;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 @RequestMapping("/medicos")
@@ -28,27 +30,37 @@ public class MedicoController {
 
     @Transactional
     @PostMapping
-    public void registrar(@RequestBody @Valid DatosRegistroMedico datos){
-        repository.save(new Medico(datos));
+    public ResponseEntity registrar(@RequestBody @Valid DatosRegistroMedico datos, UriComponentsBuilder uriComponentsBuilder){
+        var medico = new Medico(datos);
+        repository.save(medico);
+        var uri = uriComponentsBuilder.path("/medicos/{id}").buildAndExpand(medico.getId()).toUri();
+
+        return  ResponseEntity.created(uri).body(new DatosDetalleMedico(medico));
     }
 
     @GetMapping
-    public PagedModel<EntityModel<DatosListaMedico>> listar(@PageableDefault(size = 10, sort = {"nombre"}) Pageable paginacion){
+    public ResponseEntity<PagedModel<EntityModel<DatosListaMedico>>> listar(@PageableDefault(size = 10, sort = {"nombre"}) Pageable paginacion){
         Page<DatosListaMedico> pagina = repository.findAllByActivoTrue(paginacion).map(DatosListaMedico::new);
-        return pagedResourcesAssembler.toModel(pagina, datosListaMedicoModelAssembler);
+        PagedModel<EntityModel<DatosListaMedico>> pagedModel = pagedResourcesAssembler.toModel(pagina, datosListaMedicoModelAssembler);
+
+        return ResponseEntity.ok(pagedModel);
     }
 
     @Transactional
     @PutMapping
-    public void actualizar(@RequestBody @Valid DatosActualizacionMedico datos){
+    public ResponseEntity actualizar(@RequestBody @Valid DatosActualizacionMedico datos){
         var medico = repository.getReferenceById(datos.id());
         medico.actualizarInformaciones(datos);
+
+        return ResponseEntity.ok(new DatosDetalleMedico(medico));
     }
 
     @Transactional
     @DeleteMapping("/{id}")
-    public void eliminar(@PathVariable Long id){
+    public ResponseEntity eliminar(@PathVariable Long id){
         var medico = repository.getReferenceById(id);
         medico.eliminar();
+
+        return ResponseEntity.noContent().build();
     }
 }
