241af0e46892b60f3fb764267e58d2f9d4594ebd
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-06-26 12:49:09 -0300

    feat: adds listar method to PacienteController class

--- src/main/java/med/voll/api/controller/PacienteController.java
+++ src/main/java/med/voll/api/controller/PacienteController.java
@@ -1,10 +1,14 @@
 package med.voll.api.controller;
 
 import jakarta.validation.Valid;
+import med.voll.api.paciente.DatosListaPaciente;
 import med.voll.api.paciente.DatosRegistroPaciente;
 import med.voll.api.paciente.Paciente;
 import med.voll.api.paciente.PacienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -19,5 +23,10 @@ public class PacienteController {
     @Transactional
     public void registrar(@RequestBody @Valid DatosRegistroPaciente datos){
         repository.save(new Paciente(datos));
+    }
+
+    @GetMapping
+    public Page<DatosListaPaciente> listar(@PageableDefault(page = 0, size = 10, sort = {"nombre"})Pageable paginacion){
+        return repository.findAllByActivoTrue(paginacion).map(DatosListaPaciente::new);
     }
 }
