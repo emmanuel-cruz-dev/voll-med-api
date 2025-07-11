8f6bcdc565d4a6ba77daae30314c9f1aeea1efd4
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-07-11 13:08:56 -0300

    feat: adds error handlers methods to GestorDeErrores class

--- src/main/java/med/voll/api/infra/exceptions/GestorDeErrores.java
+++ src/main/java/med/voll/api/infra/exceptions/GestorDeErrores.java
@@ -1,7 +1,11 @@
 package med.voll.api.infra.exceptions;
 
 import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -19,6 +23,26 @@ public class GestorDeErrores {
     public ResponseEntity gestionarError400(MethodArgumentNotValidException ex){
         var errores = ex.getFieldErrors();
         return ResponseEntity.badRequest().body(errores.stream().map(DatosErrorValidacion::new).toList());
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity gestionarErrorBadCredentials() {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Credenciales inválidas");
+    }
+
+    @ExceptionHandler(AuthenticationException.class)
+    public ResponseEntity gestionarErrorAuthentication() {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Falla en la autenticación");
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity gestionarErrorAccesoDenegado() {
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Acceso denegado");
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity gestionarError500(Exception ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error: " +ex.getLocalizedMessage());
     }
 
     public record DatosErrorValidacion(String campo, String mensaje){
