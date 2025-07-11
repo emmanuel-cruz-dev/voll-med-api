b4f0fe743ce22fe901c140ee7869e13ad04da9ae
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-07-11 09:34:00 -0300

    feat: adds TokenService to ResponseEntity in AutenticacionController class

--- src/main/java/med/voll/api/controller/AutenticacionController.java
+++ src/main/java/med/voll/api/controller/AutenticacionController.java
@@ -2,6 +2,8 @@ package med.voll.api.controller;
 
 import jakarta.validation.Valid;
 import med.voll.api.domain.usuario.DatosAutenticacion;
+import med.voll.api.domain.usuario.Usuario;
+import med.voll.api.infra.security.TokenService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,6 +19,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class AutenticacionController {
 
     @Autowired
+    private TokenService tokenService;
+
+    @Autowired
     private AuthenticationManager manager;
 
     @PostMapping
@@ -24,7 +29,7 @@ public class AutenticacionController {
         try {
             var token = new UsernamePasswordAuthenticationToken(datos.login(), datos.contrasena());
             var autenticacion = manager.authenticate(token);
-            return ResponseEntity.ok().build();
+            return ResponseEntity.ok(tokenService.generarToken((Usuario) autenticacion.getPrincipal()));
         } catch (Exception e) {
             System.out.println("❌ Error en autenticación: " + e.getMessage());
             e.printStackTrace();
