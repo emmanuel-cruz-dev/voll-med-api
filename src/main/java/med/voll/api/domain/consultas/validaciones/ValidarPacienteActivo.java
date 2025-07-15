3572e1039de06eaff3796c0f6eaa37f8fda9ec72
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-07-15 17:02:17 -0300

    feat: adds ValidarPacienteActivo class to validaciones package

---
+++ src/main/java/med/voll/api/domain/consultas/validaciones/ValidarPacienteActivo.java
@@ -0,0 +1,17 @@
+package med.voll.api.domain.consultas.validaciones;
+
+import med.voll.api.domain.ValidacionException;
+import med.voll.api.domain.consultas.DatosReservaConsulta;
+import med.voll.api.domain.paciente.PacienteRepository;
+
+public class ValidarPacienteActivo {
+
+    private PacienteRepository repository;
+
+    public void validar(DatosReservaConsulta datos) {
+        var pacienteEstaActivo = repository.findActivoById(datos.idPaciente());
+        if(!pacienteEstaActivo) {
+            throw new ValidacionException("El paciente no se encuentra activo");
+        }
+    }
+}
