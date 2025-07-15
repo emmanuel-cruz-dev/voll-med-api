fcdb139b115d5782ca9eacb638a7a7bcd8553ed2
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-07-15 15:12:37 -0300

    feat: adds ReservaDeConsultas to ConsultaController class

--- src/main/java/med/voll/api/controller/ConsultaController.java
+++ src/main/java/med/voll/api/controller/ConsultaController.java
@@ -3,6 +3,8 @@ package med.voll.api.controller;
 import jakarta.validation.Valid;
 import med.voll.api.domain.consultas.DatosDetalleConsulta;
 import med.voll.api.domain.consultas.DatosReservaConsulta;
+import med.voll.api.domain.consultas.ReservaDeConsultas;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,9 +16,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/consultas")
 public class ConsultaController {
 
+    @Autowired
+    private ReservaDeConsultas reserva;
+
     @PostMapping
     @Transactional
     public ResponseEntity reservar(@RequestBody @Valid DatosReservaConsulta datos) {
+        reserva.reservar(datos);
         return ResponseEntity.ok(new DatosDetalleConsulta(null, null, null, null));
     }
 }
