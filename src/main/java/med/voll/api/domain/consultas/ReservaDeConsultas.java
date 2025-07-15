1b5d2245c01428443776da4c1d9aaccdd03ce565
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-07-15 16:11:07 -0300

    feat: adds elegirMedico method to ReservaDeConsultas class

--- src/main/java/med/voll/api/domain/consultas/ReservaDeConsultas.java
+++ src/main/java/med/voll/api/domain/consultas/ReservaDeConsultas.java
@@ -1,5 +1,7 @@
 package med.voll.api.domain.consultas;
 
+import med.voll.api.domain.ValidacionException;
+import med.voll.api.domain.medico.Medico;
 import med.voll.api.domain.medico.MedicoRepository;
 import med.voll.api.domain.paciente.PacienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,9 +21,28 @@ public class ReservaDeConsultas {
 
     public void reservar(DatosReservaConsulta datos) {
 
-        var medico = medicoRepository.findById(datos.idMedico()).get();
+        if(!pacienteRepository.existsById(datos.idPaciente())){
+            throw new ValidacionException("No existe un paciente con el id proporcionado");
+        }
+
+        if(datos.idMedico() != null && !medicoRepository.existsById(datos.idMedico())){
+            throw new ValidacionException("No existe un medico con el id proporcionado");
+        }
+
+        var medico = elegirMedico(datos);
         var paciente = pacienteRepository.findById(datos.idPaciente()).get();
         var consulta = new Consulta(null, medico, paciente, datos.fecha());
         consultaRepository.save(consulta);
+    }
+
+    private Medico elegirMedico(DatosReservaConsulta datos) {
+        if (datos.idMedico() != null) {
+            return medicoRepository.getReferenceById(datos.idMedico());
+        }
+        if (datos.especialidad() == null) {
+            throw new ValidacionException("Es necesario elegir una especialidad cuando no se proporciona un médico");
+        }
+
+        return medicoRepository.elegirMedicoAleatorioDisponibleEnLaFecha(datos.especialidad(), datos.fecha());
     }
 }
