01015dc153307abf12654381632432f97a0b5019
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-07-16 12:05:12 -0300

    refactor: renames classes to ValidadorConsultaConAnticipacion, ValidadorFueraHorarioConsultas and ValidadorPacienteActivo in validaciones package

--- src/main/java/med/voll/api/domain/consultas/validaciones/ValidadorPacienteActivo.java
+++ src/main/java/med/voll/api/domain/consultas/validaciones/ValidadorPacienteActivo.java
@@ -4,7 +4,7 @@ import med.voll.api.domain.ValidacionException;
 import med.voll.api.domain.consultas.DatosReservaConsulta;
 import med.voll.api.domain.paciente.PacienteRepository;
 
-public class ValidarPacienteActivo {
+public class ValidadorPacienteActivo {
 
     private PacienteRepository repository;
 
