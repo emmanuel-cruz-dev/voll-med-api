13c850f443af48c193952e933168740a38f45cbb
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-07-16 12:24:28 -0300

    feat: adds ValidadorDeConsultas interface to validaciones package

---
+++ src/main/java/med/voll/api/domain/consultas/validaciones/ValidadorDeConsultas.java
@@ -0,0 +1,7 @@
+package med.voll.api.domain.consultas.validaciones;
+
+import med.voll.api.domain.consultas.DatosReservaConsulta;
+
+public interface ValidadorDeConsultas {
+    void validar(DatosReservaConsulta datos);
+}
